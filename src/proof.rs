67d28b7238282c279186f3396891f76cc688a6b0
Andrew Whitehead <cywolf@gmail.com>
2025-02-05 14:23:44 -0800

    variable renaming and cleanups
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/proof.rs
+++ src/proof.rs
@@ -13,6 +13,28 @@ use crate::common::{
     nonzero_scalar, AccumulatorError, MembershipWitness, SetupPublic, SignedPartition,
 };
 
+#[derive(Debug, Clone)]
+struct ZKScalar {
+    secret: Scalar,
+    blind: Scalar,
+}
+
+impl ZKScalar {
+    pub fn hidden(secret: Scalar, rng: &mut impl RngCore) -> Self {
+        Self {
+            secret,
+            blind: nonzero_scalar(rng),
+        }
+    }
+
+    pub fn random(rng: &mut impl RngCore) -> Self {
+        Self {
+            secret: nonzero_scalar(rng),
+            blind: nonzero_scalar(rng),
+        }
+    }
+}
+
 impl MembershipWitness {
     /// Begin preparing a membership proof.
     pub fn prepare_proof(
@@ -20,14 +42,10 @@ impl MembershipWitness {
         pk: &SetupPublic,
         mut rng: impl RngCore,
     ) -> Result<PrepareMembershipProof, AccumulatorError> {
-        let r1 = nonzero_scalar(&mut rng);
-        let r1_b = nonzero_scalar(&mut rng);
-        let r2 = nonzero_scalar(&mut rng);
-        let r2_b = nonzero_scalar(&mut rng);
-        let r3 = nonzero_scalar(&mut rng);
-        let r3_b = nonzero_scalar(&mut rng);
-        let m = self.value;
-        let m_b = nonzero_scalar(&mut rng);
+        let r1 = ZKScalar::random(&mut rng);
+        let r2 = ZKScalar::random(&mut rng);
+        let r3 = ZKScalar::random(&mut rng);
+        let m = ZKScalar::hidden(self.value, &mut rng);
         let w = self.witness;
         let SignedPartition {
             commit: v,
@@ -36,41 +54,35 @@ impl MembershipWitness {
             epoch,
             ..
         } = &self.signature;
-        let ri = (r1 * r3).invert().unwrap();
-        let r1i = ri * r3;
-        let r3i = ri * r1;
+        let ri = (r1.secret * r3.secret).invert().unwrap();
+        let r1i = ri * r3.secret;
+        let r3i = ri * r1.secret;
 
-        let w_t = w * (r1 * r2);
-        let v_t = v * r1;
-        let u_t = u * r1;
-        let w_b = v_t * r2 - w_t * m;
-        let t1 = v_t * r2_b + w_t * m_b;
-        let s_t = s * (r3 * r1i);
-        let r_key = pk.sign_key + pk.epoch_key * Scalar::from(*epoch);
-        let r = (r_key + u) * (r1 * r3i);
-        let t2 = r * r3_b + r_key * r1_b;
-        // u'c + r(r3^) + r_key(r1^)
-        // ur1 + (r_key + u) * (r1 * r3i) + r_key(r1^)
+        let w_p = w * (r1.secret * r2.secret);
+        let v_p = v * r1.secret;
+        let u_p = u * r1.secret;
+        let w_b = v_p * r2.secret - w_p * m.secret;
+        let t1 = v_p * r2.blind + w_p * m.blind;
+        let s_p = s * (r3.secret * r1i);
+        let epoch_sign = pk.sign_key + pk.epoch_key * Scalar::from(*epoch);
+        let k_p = (epoch_sign + u) * (r1.secret * r3i);
+        let t2 = k_p * r3.blind + epoch_sign * r1.blind;
 
         Ok(PrepareMembershipProof {
             pk: pk.clone(),
             epoch: self.signature.epoch,
-            w_t,
+            w_p,
             w_b,
-            v_t,
-            u_t,
-            s_t,
-            r,
+            v_p,
+            u_p,
+            s_p,
+            k_p,
             t1,
             t2,
             r1,
-            r1_b,
             r2,
-            r2_b,
             r3,
-            r3_b,
             m,
-            m_b,
         })
     }
 }
@@ -79,38 +91,34 @@ impl MembershipWitness {
 pub struct PrepareMembershipProof {
     pk: SetupPublic,
     epoch: u32,
-    w_t: G1Projective,
+    w_p: G1Projective,
     w_b: G1Projective,
-    v_t: G1Projective,
-    u_t: G2Projective,
-    s_t: G1Projective,
-    r: G2Projective,
+    v_p: G1Projective,
+    u_p: G2Projective,
+    s_p: G1Projective,
+    k_p: G2Projective,
     t1: G1Projective,
     t2: G2Projective,
-    r1: Scalar,
-    r1_b: Scalar,
-    r2: Scalar,
-    r2_b: Scalar,
-    r3: Scalar,
-    r3_b: Scalar,
-    m: Scalar,
-    m_b: Scalar,
+    r1: ZKScalar,
+    r2: ZKScalar,
+    r3: ZKScalar,
+    m: ZKScalar,
 }
 
 impl PrepareMembershipProof {
     pub fn finalize(&self, challenge: &Scalar) -> MembershipProof {
         MembershipProof {
             epoch: self.epoch,
-            w_t: self.w_t,
+            w_p: self.w_p,
             w_b: self.w_b,
-            v_t: self.v_t,
-            u_t: self.u_t,
-            s_t: self.s_t,
-            r: self.r,
-            r1_c: self.r1_b + self.r1 * challenge,
-            r2_c: self.r2_b - self.r2 * challenge,
-            r3_c: self.r3_b - self.r3 * challenge,
-            m_c: self.m_b + self.m * challenge,
+            v_p: self.v_p,
+            u_p: self.u_p,
+            s_p: self.s_p,
+            k_p: self.k_p,
+            r1_c: self.r1.blind + self.r1.secret * challenge,
+            r2_c: self.r2.blind - self.r2.secret * challenge,
+            r3_c: self.r3.blind - self.r3.secret * challenge,
+            m_c: self.m.blind + self.m.secret * challenge,
         }
     }
 
@@ -124,12 +132,12 @@ impl PrepareMembershipProof {
         // );
         self.pk.add_challenge_input(out);
         out.extend_from_slice(&self.epoch.to_le_bytes());
-        out.extend_from_slice(&self.w_t.to_compressed());
+        out.extend_from_slice(&self.w_p.to_compressed());
         out.extend_from_slice(&self.w_b.to_compressed());
-        out.extend_from_slice(&self.v_t.to_compressed());
-        out.extend_from_slice(&self.u_t.to_compressed());
-        out.extend_from_slice(&self.s_t.to_compressed());
-        out.extend_from_slice(&self.r.to_compressed());
+        out.extend_from_slice(&self.v_p.to_compressed());
+        out.extend_from_slice(&self.u_p.to_compressed());
+        out.extend_from_slice(&self.s_p.to_compressed());
+        out.extend_from_slice(&self.k_p.to_compressed());
         out.extend_from_slice(&self.t1.to_compressed());
         out.extend_from_slice(&self.t2.to_compressed());
     }
@@ -145,12 +153,12 @@ impl PrepareMembershipProof {
 #[derive(Debug, Clone)]
 pub struct MembershipProof {
     epoch: u32,
-    w_t: G1Projective,
+    w_p: G1Projective,
     w_b: G1Projective,
-    v_t: G1Projective,
-    u_t: G2Projective,
-    s_t: G1Projective,
-    r: G2Projective,
+    v_p: G1Projective,
+    u_p: G2Projective,
+    s_p: G1Projective,
+    k_p: G2Projective,
     r1_c: Scalar,
     r2_c: Scalar,
     r3_c: Scalar,
@@ -165,24 +173,27 @@ impl MembershipProof {
         let check_challenge = ch.ct_eq(&challenge);
 
         // Combining the pairings using randomness
-        // e(S', R) =? e(G1, G2)
+        // e(S', K') =? e(G1, G2)
         // e(W', G2a) =? e(Wb, G2)
         // e(G1, U') =? e(V', G2)
-        // => e(S', R)•e(W'c, G2a)•e(G1c^2, U') =? e(G1 + Wbc + V'c^2, G2)
+        // => e(S', K')•e(W'c, G2a)•e(G1c^2, U') =? e(G1 + Wbc + V'c^2, G2)
 
         let neg_g2p = G2Prepared::from(-G2Affine::generator());
         let pair = multi_miller_loop(&[
-            (&self.s_t.to_affine(), &G2Prepared::from(self.r.to_affine())),
             (
-                &(self.w_t * challenge).to_affine(),
+                &self.s_p.to_affine(),
+                &G2Prepared::from(self.k_p.to_affine()),
+            ),
+            (
+                &(self.w_p * challenge).to_affine(),
                 &G2Prepared::from(pk.member_key),
             ),
             (
                 &(G1Affine::generator() * challenge.square()).to_affine(),
-                &G2Prepared::from(self.u_t.to_affine()),
+                &G2Prepared::from(self.u_p.to_affine()),
             ),
             (
-                &(G1Affine::generator() + self.w_b * challenge + self.v_t * challenge.square())
+                &(G1Affine::generator() + self.w_b * challenge + self.v_p * challenge.square())
                     .to_affine(),
                 &neg_g2p,
             ),
@@ -200,19 +211,19 @@ impl MembershipProof {
     /// Add the challenge input to a shared transcript.
     // FIXME: would be better to use a standard trait here
     pub fn add_challenge_input(&self, pk: &SetupPublic, challenge: Scalar, out: &mut Vec<u8>) {
-        let r_key = pk.sign_key + pk.epoch_key * Scalar::from(self.epoch);
-        let t1 = self.w_b * challenge + self.v_t * self.r2_c + self.w_t * self.m_c;
-        let t2 = self.u_t * challenge + self.r * self.r3_c + r_key * self.r1_c;
+        let epoch_sign = pk.sign_key + pk.epoch_key * Scalar::from(self.epoch);
+        let t1 = self.w_b * challenge + self.v_p * self.r2_c + self.w_p * self.m_c;
+        let t2 = self.u_p * challenge + self.k_p * self.r3_c + epoch_sign * self.r1_c;
         // println!("t1v: {:?}, t2v: {:?}", t1.to_affine(), t2.to_affine());
 
         pk.add_challenge_input(out);
         out.extend_from_slice(&self.epoch.to_le_bytes());
-        out.extend_from_slice(&self.w_t.to_compressed());
+        out.extend_from_slice(&self.w_p.to_compressed());
         out.extend_from_slice(&self.w_b.to_compressed());
-        out.extend_from_slice(&self.v_t.to_compressed());
-        out.extend_from_slice(&self.u_t.to_compressed());
-        out.extend_from_slice(&self.s_t.to_compressed());
-        out.extend_from_slice(&self.r.to_compressed());
+        out.extend_from_slice(&self.v_p.to_compressed());
+        out.extend_from_slice(&self.u_p.to_compressed());
+        out.extend_from_slice(&self.s_p.to_compressed());
+        out.extend_from_slice(&self.k_p.to_compressed());
         out.extend_from_slice(&t1.to_compressed());
         out.extend_from_slice(&t2.to_compressed());
     }
